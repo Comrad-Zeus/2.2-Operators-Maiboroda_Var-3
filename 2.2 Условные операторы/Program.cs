6f03194d81809e1bc88ea5cc063808f5b6795063
Comrad-Zeus <113071286+Comrad-Zeus@users.noreply.github.com>
2022-10-09 14:37:18 +0200

    Add files via upload

---
+++ 2.2 Условные операторы/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2.2 Условные операторы Вариант 3 - Майборода Анатолий - Сложный 
+
+Console.WriteLine("Введите 2х значное число: ");
+ushort n = Convert.ToUInt16(Console.ReadLine());
+int x = n % 10;
+int y = n / 10;
+if ((x == 3 || x == 7) && (y == 3 || y == 7))
+{
+    Console.WriteLine("Имеет цифры 3 и 7");
+}
+
+if ((x == 4 && y == 8) || (x == 8 && y == 4) || x == 9 || y == 9)
+{
+    Console.WriteLine("Имеет цифры 4 и 8 или 9");
+}
